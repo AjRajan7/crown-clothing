d13d55ed686005e89c6311c7266d650ac11334d0
AjRajan7 <newacct7robot@gmail.com>
2020-07-07 10:12:49 +0530

    Updated with shop page and collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage=(props)=>(
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
         <Route  exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
     </div>
   );
 }
